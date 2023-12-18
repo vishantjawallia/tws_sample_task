2d650fd51baf723eff02b827d5b5c31798f1095b
Vishant Jawallia <68182707+vishantjawallia@users.noreply.github.com>
2023-12-18 11:38:57 +0530

    error fix

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import '../views/dashboard_screen/dashboard_screen_view.dart';
+import '../views/dashboard/dashboard_view.dart';
 import '../views/splash/splash_view.dart';
 
 class Routes {
   static final List<GetPage<dynamic>> getRoutes = [
-    GetPage(
-        name: DashboardScreenView.routeName, page: () => DashboardScreenView()),
+    GetPage(name: DashboardView.routeName, page: () => DashboardView()),
     GetPage(name: SplashView.routeName, page: () => const SplashView()),
   ];
   static Map<String, Widget Function(BuildContext)> routes = {
-    DashboardScreenView.routeName: (p0) => DashboardScreenView(),
+    DashboardView.routeName: (p0) => DashboardView(),
     SplashView.routeName: (p0) => const SplashView(),
   };
 }
